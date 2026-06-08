b285205d56220b8c173ab9ba0a8aa20503fb4222
Jorge Diaz-Delgado Carrillo <85868623+jorgeddc@users.noreply.github.com>
2026-06-08 07:16:40 +0200

    Update vite.config.ts  base: '/Focus-Time/'  //

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/Focus-Time/'  // ← nombre exacto de tu repositorio
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
+
